1b155db4c3e9cb751f24ad2e3480d154ed1a8467
Le Hai Son <lehaisonmath6@gmail.com>
2019-07-29 22:35:50 +0700

    oke

--- EtcdEndpointManager.go
+++ EtcdEndpointManager.go
@@ -2,9 +2,11 @@ package GoEndpointManager
 
 import (
 	"strings"
-	etcdv3 "github.com/coreos/etcd/clientv3"
+	// etcdv3 "github.com/coreos/etcd/clientv3"
 	"context"
 	"sync"
+
+	etcdv3 "go.etcd.io/etcd/clientv3"
 )
 
 // //EnpointManagerIf interface of enpoint manager
@@ -17,41 +19,41 @@ import (
 type EtcdEndpointManager struct {
 	InMemEndpointManager
 	EtcdEndpoints []string
-	client *etcdv3.Client
+	client        *etcdv3.Client
 
-	rootService string 
-	EndpointsMap sync.Map //value is an array of Endpoint : []*Endpoint
+	rootService      string
+	EndpointsMap     sync.Map //value is an array of Endpoint : []*Endpoint
 	EndpointRotating sync.Map // map from serviceID to int
 }
 
 //GetEndpoint (serviceID string) (host, port string, err error)
-func (o *EtcdEndpointManager) GetEndpoint(serviceID string) (host, port string, err error){
-	if (o.client == nil ){
+func (o *EtcdEndpointManager) GetEndpoint(serviceID string) (host, port string, err error) {
+	if o.client == nil {
 		o.Start()
 	}
 	/*
-	Get in Endpoint map first
-	If it does not exist, get from etcd, and monitor it.  
+		Get in Endpoint map first
+		If it does not exist, get from etcd, and monitor it.
 	*/
-	endpoints, ok := o.EndpointsMap.Load(serviceID) 
-	rotated , _ := o.EndpointRotating.LoadOrStore(serviceID, 0)
-	o.EndpointRotating.Store( serviceID, rotated.(int) +1)
+	endpoints, ok := o.EndpointsMap.Load(serviceID)
+	rotated, _ := o.EndpointRotating.LoadOrStore(serviceID, 0)
+	o.EndpointRotating.Store(serviceID, rotated.(int)+1)
 	if ok {
 		arr := endpoints.([]*Endpoint)
-		if len(arr) > 0  {
+		if len(arr) > 0 {
 			iPos := rotated.(int) % len(arr)
-			host, port = arr[iPos].Host, arr[iPos].Port;
+			host, port = arr[iPos].Host, arr[iPos].Port
 			err = nil
 			return
 		} else {
 			return o.InMemEndpointManager.GetEndpoint(serviceID)
 		}
-	} else { 
+	} else {
 		// try to get from etcd
-		resp, gerr := o.client.Get(context.Background(), serviceID);
-		if (gerr == nil){
+		resp, gerr := o.client.Get(context.Background(), serviceID)
+		if gerr == nil {
 			for _, kv := range resp.Kvs {
-				if string (kv.Key) == serviceID {
+				if string(kv.Key) == serviceID {
 					Eps := o.parseServiceFromString(serviceID, string(kv.Value))
 					if len(Eps) > 0 {
 						host, port, err = Eps[0].Host, Eps[0].Port, nil
@@ -65,37 +67,34 @@ func (o *EtcdEndpointManager) GetEndpoint(serviceID string) (host, port string, 
 			return o.InMemEndpointManager.GetEndpoint(serviceID)
 		}
 	}
-	return 
+	return
 }
 
 //SetDefaultEntpoint Set default endpoint manager
 func (o *EtcdEndpointManager) SetDefaultEntpoint(serviceID, host, port string) (err error) {
 	o.InMemEndpointManager.SetDefaultEntpoint(serviceID, host, port)
 
-
-	if (o.client != nil){
+	if o.client != nil {
 		//Already connected to etcdserver
-		o. GetEndpoint(serviceID)
+		o.GetEndpoint(serviceID)
 	}
 
-	return 
+	return
 }
 
 //NewEtcdEndpointManager Create endpoint manager
-func NewEtcdEndpointManager( etcdConfigHostports []string ) *EtcdEndpointManager{
+func NewEtcdEndpointManager(etcdConfigHostports []string) *EtcdEndpointManager {
 
-
-	o:= &EtcdEndpointManager{
+	o := &EtcdEndpointManager{
 		InMemEndpointManager: InMemEndpointManager{
-			defaultEndpoints:  make( map[string]*Endpoint ),
+			defaultEndpoints: make(map[string]*Endpoint),
 		},
-		EtcdEndpoints : etcdConfigHostports,
-		client: nil,
+		EtcdEndpoints: etcdConfigHostports,
+		client:        nil,
 	}
-	
+
 	return o
 }
-
 
 func (o *EtcdEndpointManager) Start() bool {
 	if o.client != nil {
@@ -103,17 +102,17 @@ func (o *EtcdEndpointManager) Start() bool {
 	}
 
 	cfg := etcdv3.Config{
-		Endpoints:               o.EtcdEndpoints,		
+		Endpoints: o.EtcdEndpoints,
 	}
-	aClient , err:= etcdv3.New(cfg)
+	aClient, err := etcdv3.New(cfg)
 	if err != nil {
 		return false
 	}
-	o.client = aClient;
+	o.client = aClient
 
 	opts := []etcdv3.OpOption{etcdv3.WithPrefix()}
-	if len (o.rootService) > 0 {
-		
+	if len(o.rootService) > 0 {
+
 		rootWatchan := aClient.Watch(context.Background(), o.rootService, opts...)
 		go o.MonitorChan(rootWatchan)
 	}
@@ -122,18 +121,18 @@ func (o *EtcdEndpointManager) Start() bool {
 		serviceChan := aClient.Watch(context.Background(), serviceID, nil...)
 		go o.MonitorChan(serviceChan)
 	}
-	
+
 	return true
 }
 
 func (o *EtcdEndpointManager) parseServiceFromString(serviceID, val string) []*Endpoint {
-	HostPorts := strings.Split(val,",");
-	var Eps []*Endpoint 
-	for _, HostPort:= range HostPorts {
-		hp := strings.Split(HostPort,":")
-		if len (hp) == 2 {
-			aHost := strings.Trim(hp[0], " ");
-			aPort := strings.Trim(hp[1], " ");
+	HostPorts := strings.Split(val, ",")
+	var Eps []*Endpoint
+	for _, HostPort := range HostPorts {
+		hp := strings.Split(HostPort, ":")
+		if len(hp) == 2 {
+			aHost := strings.Trim(hp[0], " ")
+			aPort := strings.Trim(hp[1], " ")
 			Eps = append(Eps, &Endpoint{aHost, aPort})
 		}
 	}
@@ -142,15 +141,15 @@ func (o *EtcdEndpointManager) parseServiceFromString(serviceID, val string) []*E
 }
 
 //MonitorChan monitor an etcd watcher channel
-func (o *EtcdEndpointManager) MonitorChan(wchan etcdv3.WatchChan ) {
+func (o *EtcdEndpointManager) MonitorChan(wchan etcdv3.WatchChan) {
 	for wresp := range wchan {
 		for _, ev := range wresp.Events {
 			//fmt.Printf("Watch V3 .... %s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
 			if ev.Type == etcdv3.EventTypePut {
-				val := string(ev.Kv.Value);
+				val := string(ev.Kv.Value)
 				serviceID := string(ev.Kv.Key)
 				o.parseServiceFromString(serviceID, val)
 			}
 		}
-	}	
+	}
 }
