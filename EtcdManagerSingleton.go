b15eaa32bda89071981416ebeffeb80123da60d2
lehaisonmath6 <lehaisonmath6@gmail.com>
2020-03-24 13:59:13 +0700

    Update new

--- EtcdManagerSingleton.go
+++ EtcdManagerSingleton.go
@@ -8,6 +8,21 @@ var once sync.Once
 func GetEtcdBackendEndpointManagerSingleton(etcdEndpoints []string) *EtcdBackendEndpointManager {
 	once.Do(func() {
 		etcdManagerSingleton = NewEtcdBackendEndpointManager(etcdEndpoints)
+fmt.Println("Starting Backend Endpoint manager etcd  ", o.EtcdEndpoints)
+
+
+	if len(o.EtcdEndpoints) == 0 {
+		etcdManagerSingleton = nil
+	}
+
+	cfg := etcdv3.Config{
+		Endpoints: o.EtcdEndpoints,
+	}
+	aClient, err := etcdv3.New(cfg)
+	if err != nil {
+		etcdManagerSingleton = nil
+	}
+	o.client = aClient
 	})
 	return etcdManagerSingleton
 }
